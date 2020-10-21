d8e4f8916e9d07d94c12a6936fdf36ca1e35ae9c
Magdalena Świderska <m.swiderska@outlook.com>
2020-10-21 15:29:54 +0200

    Add users list & join&left info

--- client/app.js
+++ client/app.js
@@ -7,7 +7,14 @@ const messageContentInput = document.getElementById("message-content");
 
 const socket = io();
 
-socket.on('message', ({ author, content }) => addMessage(author, content))
+socket.on('message', ({ author, content }) => addMessage(author, content));
+
+socket.on('join', ({ name }) =>
+  addMessage('Chat Bot', `${name} has joined the chat`)
+);
+socket.on('removeUser', ({ name }) =>
+  addMessage('Chat Bot', `${name} has left the chat`)
+);
 
 let userName = '';
 
@@ -19,28 +26,34 @@ addMessageForm.addEventListener('submit', (event) => {
     sendMessage(event);
 });
 
-const login = () => {
+const login = event => {
     event.preventDefault();
 
     if (userNameInput.value == '')  {
-        alert('Type your name')
-    }
-    else {
+        alert('Type your name');
+    } else {
         userName = userNameInput.value;
         loginForm.classList.remove('show');
         messagesSection.classList.add('show');
+        socket.emit('join', {
+            name: userName,
+            id: socket.id
+        });
     }
 };
 
-const sendMessage = () => {
+const sendMessage = event => {
     event.preventDefault();
 
     if (messageContentInput.value == '') {
-        alert('Type your message, please')
+        alert('Type your message, please');
       }
       else {
         addMessage(userName, messageContentInput.value);
-        socket.emit('message', { author: userName, content: messageContentInput.value })
+        socket.emit('message', { 
+            author: userName, 
+            content: messageContentInput.value 
+        });
         messageContentInput.value = '';
       }
     };
@@ -48,7 +61,11 @@ const sendMessage = () => {
     const addMessage = (author, content) => {
       const message = document.createElement('li');
       message.classList.add('message', 'message--received');
-      if (author == userName) message.classList.add('message--self');
+      if (author == userName) { 
+          message.classList.add('message--self');
+      } else if (author == 'Chat Bot') {
+        message.classList.add('message--info');
+    }
       message.innerHTML = `
         <h3 class="message__author">${userName === author ? 'You' : author }</h3>
         <div class="message__content">
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const socket = require('socket.io');
 const app = express();
 
 const messages = [];
+let users = [];
 
 app.use(express.static(path.join(__dirname + '/client')));
 
@@ -18,15 +19,31 @@ const server = app.listen(8000, () => {
 
 const io = socket(server);
 
-io.on('connection', (socket) => {
+io.on('connection', socket => {
     console.log('New client! Its id – ' + socket.id);
-    socket.on('message', (message) => {
+
+    socket.on('join', user => {
+        console.log('New user has joined ' + socket.id);
+        users.push(user);
+        socket.broadcast.emit('join', user);
+});
+
+    socket.on('message', message => {
         console.log('Oh, I\'ve got something from ' + socket.id);
         messages.push(message);
         socket.broadcast.emit('message', message);
       });
-    socket.on('disconnect', () => { console.log('Oh, socket ' + socket.id + ' has left') });
+
+    socket.on('disconnect', () => { 
+        console.log('Oh, socket ' + socket.id + ' has left');
+        const leavingUser = users.find(user => user.id == socket.id);
+        console.log(leavingUser, users);
+        users = users.filter(user => user.id !== socket.id);
+        socket.broadcast.emit('removeUser', leavingUser);
+        console.log(users);
+    });
+
     console.log('I\'ve added a listener on message and disconnect events \n');
-  });
+});
 
   
